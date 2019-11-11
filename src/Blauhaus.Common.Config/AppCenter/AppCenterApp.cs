91f5896b6e9986435996edebdaf5c1aa4252779a
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2019-11-11 20:03:19 +0200

    no message

---
+++ src/Blauhaus.Common.Config/AppCenter/AppCenterApp.cs
@@ -0,0 +1,16 @@
+﻿namespace Blauhaus.Common.Config.AppCenter
+{
+    public class AppCenterApp
+    {
+        public AppCenterApp(string appName, string appSecret, bool isPushEnabled = true)
+        {
+            AppName = appName;
+            AppSecret = appSecret;
+            IsPushEnabled = isPushEnabled;
+        }
+
+        public string AppName { get; }
+        public string AppSecret { get; }
+        public bool IsPushEnabled { get;  } 
+    }
+}
---
+++ src/Blauhaus.Common.Config/AppCenter/IAppCenterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Blauhaus.Common.ValueObjects.RuntimePlatforms;
+
+namespace Blauhaus.Common.Config.AppCenter
+{
+    public interface IAppCenterConfig
+    {
+        string ApiToken { get; }
+        string OrganizationName { get; } 
+
+        Dictionary<RuntimePlatform, AppCenterApp> Apps { get; }
+    }
+}
