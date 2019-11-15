b91044ed9164fdc71854238f621c7a4865a5df4b
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2019-11-15 10:01:08 +0200

    no message

--- src/Blauhaus.Common.ValueObjects/RuntimePlatforms/RuntimePlatform.cs
+++ src/Blauhaus.Common.ValueObjects/RuntimePlatforms/RuntimePlatform.cs
@@ -1,4 +1,5 @@
-﻿using Blauhaus.Common.ValueObjects._Base;
+﻿using System;
+using Blauhaus.Common.ValueObjects._Base;
 
 namespace Blauhaus.Common.ValueObjects.RuntimePlatforms
 {
@@ -11,5 +12,14 @@ namespace Blauhaus.Common.ValueObjects.RuntimePlatforms
         public static RuntimePlatform Android => new RuntimePlatform(nameof(Android));
         public static RuntimePlatform iOS => new RuntimePlatform(nameof(iOS));
         public static RuntimePlatform UWP => new RuntimePlatform(nameof(UWP));
+
+        public static RuntimePlatform FromString(string value)
+        {
+            if (value.ToLowerInvariant().Equals("android")) return Android;
+            if (value.ToLowerInvariant().Equals("uwp")) return UWP;
+            if (value.ToLowerInvariant().Equals("ios")) return iOS;
+
+            throw new ArgumentException($"Platform string \'{value}\' not recognized. ");
+        }
     }
 }
