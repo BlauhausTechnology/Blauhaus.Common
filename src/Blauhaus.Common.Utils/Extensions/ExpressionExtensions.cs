afdd996d984f02f4a83ee5314d7da5db80214761
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2019-12-04 14:44:38 +0200

    no message

---
+++ src/Blauhaus.Common.Utils/Extensions/ExpressionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace Blauhaus.Common.Utils.Extensions
+{
+    public static class ExpressionExtensions
+    {
+        public static string ToPropertyName<T>(this Expression<T> expression)
+        {
+            var expressionBody = expression.Body.ToString();
+            return expressionBody.Substring(expressionBody.IndexOf('.') + 1);
+        }
+    }
+}
