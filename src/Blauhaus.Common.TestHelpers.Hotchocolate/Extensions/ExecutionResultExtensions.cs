d3edc18ed46611caac9e51c5420a4eb283d919c3
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2019-12-05 17:01:42 +0200

    no message

---
+++ src/Blauhaus.Common.TestHelpers.Hotchocolate/Extensions/ExecutionResultExtensions.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using Blauhaus.Common.Utils.Extensions;
+using HotChocolate.Execution;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+
+namespace Blauhaus.Common.TestHelpers.Hotchocolate.Extensions
+{
+    public static class ExecutionResultExtensions
+    {
+        public static IExecutionResult VerifyContainsNoData(this IExecutionResult executionResult)
+        {
+            Assert.That(((ReadOnlyQueryResult)executionResult).Data.Count, Is.EqualTo(0));
+            return executionResult;
+        } 
+
+        public static IExecutionResult VerifyNoErrors(this IExecutionResult executionResult)
+        {
+            var errors = executionResult.Errors;
+
+            if (errors.Count > 0)
+            {
+                foreach (var error in errors)
+                {
+                    Assert.Fail(error.Message);
+                }
+            }
+            return executionResult;
+        } 
+
+        public static IExecutionResult VerifyException(this IExecutionResult executionResult, string exceptionErrorMessage)
+        {
+            var errors = executionResult.Errors;
+
+            if (errors.Count == 0)
+            {
+                Assert.Fail("No errors were contained in the ExecutionResult");
+            }
+
+            var expectedError = errors.FirstOrDefault(x => x.Exception?.Message == exceptionErrorMessage);
+
+            if(expectedError == null)
+                Assert.Fail($"No errors contained the expected exception message {exceptionErrorMessage}");
+            else
+                Assert.That(exceptionErrorMessage, Is.EqualTo(exceptionErrorMessage));
+
+            return executionResult;
+        } 
+
+        public static JObject ExtractData(this IExecutionResult executionResult)
+        {
+            var result = (ReadOnlyQueryResult) executionResult;
+            
+            using var stream = new MemoryStream();
+            
+            var resultSerializer = new JsonQueryResultSerializer();
+            resultSerializer.SerializeAsync(
+                result, stream).Wait();
+            var jsonString =  Encoding.UTF8.GetString(stream.ToArray());
+            
+            var jObject = JObject.Parse(jsonString);
+
+            var data = (JObject) jObject.GetValue("data");
+
+            return data;
+        }
+
+        public static OrderedDictionary GetPropertyDictionary(this IExecutionResult executionResult, string objectName)
+        {
+            var result = (ReadOnlyQueryResult)executionResult;
+
+            var index = -1;
+            var foundKey=false;
+            
+            foreach (var key in result.Data.Keys)
+            {
+                index++;
+                if (key == objectName)
+                {
+                    foundKey = true;
+                    break;
+                }
+            }
+
+            if(!foundKey)
+                Assert.Fail($"ExecutionResult does not contain an object called {objectName}");
+            
+            return  (OrderedDictionary)result.Data.Values.ToArray()[index];
+        }
+        
+        public static OrderedDictionary GetPropertyDictionary<T>(this IExecutionResult executionResult, Expression<Func<T, object>> expression)
+        {
+           return executionResult.GetPropertyDictionary(expression.ToPropertyName());
+        }
+    }
+}
---
+++ src/Blauhaus.Common.TestHelpers.Hotchocolate/Extensions/OrderedDictionaryExtensions.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Linq.Expressions;
+using Blauhaus.Common.Utils.Extensions;
+using HotChocolate.Execution;
+using NUnit.Framework;
+
+namespace Blauhaus.Common.TestHelpers.Hotchocolate.Extensions
+{
+    public static class OrderedDictionaryExtensions
+    {
+        
+        public static object GetProperty<T>(this OrderedDictionary orderedDictionary, Expression<Func<T, object>> expression)
+        {
+            return orderedDictionary.GetProperty(expression.ToPropertyName());
+        }
+
+
+        public static object GetProperty(this OrderedDictionary orderedDictionary, string propertyName)
+        {
+            var index = -1;
+            var foundKey=false;
+            
+            foreach (var key in orderedDictionary.Keys)
+            {
+                index++;
+                if (key == propertyName)
+                {
+                    foundKey = true;
+                    break;
+                }
+            }
+
+            if(!foundKey)
+                Assert.Fail($"The key {propertyName} was not found");
+
+            return  orderedDictionary.Values.ToArray()[index];
+        }
+        
+        public static void VerifyProperty<T, TProperty>(this OrderedDictionary orderedDictionary, Expression<Func<T, TProperty>>  expression, TProperty propertyValue)
+        {
+            var propertyName = expression.ToPropertyName();
+            var property = orderedDictionary.GetProperty(propertyName);
+
+            if(typeof(TProperty) == typeof(Guid?))
+            {
+                Assert.That( Guid.Parse(property.ToString()), Is.EqualTo(propertyValue));
+            }
+            else
+            {
+                Assert.That(property, Is.EqualTo(propertyValue));
+            }
+        }
+        
+
+        public static void VerifyProperty(this OrderedDictionary orderedDictionary,  string propertyName, object propertyValue)
+        {
+            var property = orderedDictionary.GetProperty(propertyName);
+            Assert.That(property, Is.EqualTo(propertyValue));
+        }
+        
+        public static void VerifyPropertyDateTimeOffset(this OrderedDictionary orderedDictionary,  string propertyName, DateTimeOffset propertyValue)
+        {
+            var objectProperty = (string)orderedDictionary.GetProperty(propertyName);
+            Assert.That(objectProperty, Is.EqualTo(propertyValue.ToString("yyyy-MM-ddTHH\\:mm\\:ss.fffZ", CultureInfo.InvariantCulture)));
+        }
+
+        public static void VerifyPropertyDateTime(this OrderedDictionary orderedDictionary,  string propertyName, DateTime propertyValue)
+        {
+            var objectProperty = (string)orderedDictionary.GetProperty(propertyName);
+            Assert.That(objectProperty, Is.EqualTo(propertyValue.ToString("yyyy-MM-ddTHH\\:mm\\:ss.fffZ", CultureInfo.InvariantCulture)));
+        }
+
+        public static void VerifyPropertyGuid(this OrderedDictionary orderedDictionary,  string propertyName, Guid propertyValue)
+        {
+            var objectProperty = (string)orderedDictionary.GetProperty(propertyName);
+            Assert.That(Guid.Parse(objectProperty), Is.EqualTo(propertyValue));
+        }
+
+    }
+}
