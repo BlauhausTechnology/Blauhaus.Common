dee665ce76cbca367dd5f655ce4f3b1f885d0d65
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2019-12-15 13:16:14 +0200

    ICommandResult<TPayload>

---
+++ src/Blauhaus.Common.Domain/CommandResults/BaseCommandResult.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Blauhaus.Common.Domain.CommandResults
+{
+    public abstract class BaseCommandResult<TResult, TPayload> : ICommandResult<TPayload> where TResult : BaseCommandResult<TResult, TPayload>, new()
+    {
+        public TPayload Payload { get; set; }
+        public List<string> UserErrors { get; set; } = new List<string>();
+
+        public static TResult Success(TPayload payload) => new TResult{Payload = payload};
+        public static TResult Error(params string[] userErrors) => new TResult{UserErrors = userErrors.ToList()};
+    }
+}
---
+++ src/Blauhaus.Common.Domain/CommandResults/ICommandResult.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Blauhaus.Common.Domain.CommandResults
+{
+    public interface ICommandResult<TPayload>
+    {
+        TPayload Payload { get; set; }
+        List<string> UserErrors { get; set; }
+    }
+}
