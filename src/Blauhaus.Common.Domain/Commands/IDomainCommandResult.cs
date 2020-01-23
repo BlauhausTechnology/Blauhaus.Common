6b0125e8ccaea5a6f257284465a6c75022b45138
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-01-23 16:33:36 +0200

    DomainCommand and DomainQuery abstractions

---
+++ src/Blauhaus.Common.Domain/Commands/IDomainCommandResult.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Blauhaus.Common.Domain.Commands
+{
+    public interface IDomainCommandResult<TPayload>
+    {
+        TPayload Payload { get; set; }
+        List<DomainCommandError> Errors { get; set; }
+    }
+}
