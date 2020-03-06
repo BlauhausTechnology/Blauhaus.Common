76e0d62845fec30c7e5b2116ccf2aff47f01919a
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-03-06 17:32:47 +0200

    extended value objects for analytics types

---
+++ src/Blauhaus.Common.ValueObjects/DeviceType/DeviceType.cs
@@ -0,0 +1,21 @@
+﻿using Blauhaus.Common.ValueObjects._Base;
+using Blauhaus.Common.ValueObjects.RuntimePlatforms;
+
+namespace Blauhaus.Common.ValueObjects.DeviceType
+{
+    public class DeviceType : BaseValueObject<IDeviceType, string>, IDeviceType
+    {
+        protected DeviceType(string value) : base(value)
+        {
+        }
+
+        
+        public static DeviceType Unknown => new DeviceType(nameof(Unknown));
+        public static DeviceType Phone => new DeviceType(nameof(Phone));
+        public static DeviceType Tablet => new DeviceType(nameof(Tablet));
+        public static DeviceType PC => new DeviceType(nameof(PC));
+        public static DeviceType TV => new DeviceType(nameof(TV));
+        public static DeviceType Watch => new DeviceType(nameof(Watch));
+        public static DeviceType Server => new DeviceType(nameof(Server));
+    }
+}
---
+++ src/Blauhaus.Common.ValueObjects/DeviceType/IDeviceType.cs
@@ -0,0 +1,9 @@
+﻿using Blauhaus.Common.ValueObjects._Base;
+
+namespace Blauhaus.Common.ValueObjects.DeviceType
+{
+    public interface IDeviceType : IValueObject<IDeviceType, string>
+    {
+        
+    }
+}
