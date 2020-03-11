58973ff4d53ecd375c3c8edb398722a85922004f
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-03-11 12:53:11 +0200

    added http testhelpers

---
+++ src/Blauhaus.Common.TestHelpers.Http/MockBuilders/HttpClientFactoryMockBuilder.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http;
+using Moq;
+
+namespace Blauhaus.Common.TestHelpers.Http.MockBuilders
+{
+    public class HttpClientFactoryMockBuilder : BaseMockBuilder<HttpClientFactoryMockBuilder, IHttpClientFactory>
+    {
+
+        public HttpClientFactoryMockBuilder Where_CreateClient_returns(HttpClient client, string clientName = "")
+        {
+            Mock.Setup(x => x.CreateClient(string.IsNullOrEmpty(clientName) ? It.IsAny<string>() : clientName))
+                .Returns(client);
+
+            return this;
+        }
+    }
+}
