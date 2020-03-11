945a1f8862b15f8c354e016a9e481bc0533a77f2
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-03-11 13:29:26 +0200

    fixed http mock builder

--- src/Blauhaus.Common.TestHelpers.Http/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Blauhaus.Common.TestHelpers.Http
-{
-    public class Class1
-    {
-    }
-}
--- src/Blauhaus.Common.TestHelpers.Http/MockBuilders/HttpClientFactoryMockBuilder.cs
+++ src/Blauhaus.Common.TestHelpers.Http/MockBuilders/HttpClientFactoryMockBuilder.cs
@@ -8,8 +8,16 @@ namespace Blauhaus.Common.TestHelpers.Http.MockBuilders
 
         public HttpClientFactoryMockBuilder Where_CreateClient_returns(HttpClient client, string clientName = "")
         {
-            Mock.Setup(x => x.CreateClient(string.IsNullOrEmpty(clientName) ? It.IsAny<string>() : clientName))
-                .Returns(client);
+            if (string.IsNullOrEmpty(clientName))
+            {
+                Mock.Setup(x => x.CreateClient(It.IsAny<string>()))
+                    .Returns(client);
+            }
+            else
+            {
+                Mock.Setup(x => x.CreateClient(clientName))
+                    .Returns(client);
+            }
 
             return this;
         }
