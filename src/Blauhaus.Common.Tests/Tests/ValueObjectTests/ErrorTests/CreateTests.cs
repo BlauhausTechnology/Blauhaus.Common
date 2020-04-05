6f8ba7d0e64118dad96d439e9019204908543ca8
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-04-05 08:42:03 +0200

    Error

---
+++ src/Blauhaus.Common.Tests/Tests/ValueObjectTests/ErrorTests/CreateTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace Blauhaus.Common.Tests.Tests.ValueObjectTests.ErrorTests
+{
+    public class CreateTests
+    {
+        [Test]
+        public void SHOULD_use_name_of_property_as_code()
+        {
+            var resultOne = TestErrors.TestErrorOne;
+            var resultTwo = TestErrors.TestErrorTwo;
+
+            Assert.AreEqual(resultOne.Code, "TestErrorOne");
+            Assert.AreEqual(resultOne.Description, "Description One");
+            Assert.AreEqual(resultTwo.Code, "TestErrorTwo");
+            Assert.AreEqual(resultTwo.Description, "Description Two");
+        }
+    }
+}
---
+++ src/Blauhaus.Common.ValueObjects/Errors/Error.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Blauhaus.Common.ValueObjects._Base;
+
+namespace Blauhaus.Common.ValueObjects.Errors
+{
+    public class Error : BaseValueObject<Error>
+    {
+        public Error(string code, string description)
+        {
+            Code = code;
+            Description = description;
+        }
+
+        public string Code { get; }
+        public string Description { get; }
+
+        public static Error Create(string errorDescription, [CallerMemberName] string errorCode = "")
+        {
+            return new Error(errorCode, errorDescription);
+        }
+
+        public static Error Deserialize(string serializedError)
+        {
+            var deserialized = serializedError.Split(new []{" ::: "}, StringSplitOptions.None);
+            if (deserialized.Length != 2)
+            {
+                throw new ArgumentException($"Input {serializedError} is not a valid serialized Error");
+            }
+            return new Error(deserialized[0], deserialized[1]);
+        }
+        
+        public override string ToString()
+        {
+            return $"{Code} ::: {Description}";
+        }
+
+
+        protected override int GetHashCodeCore()
+        {
+            unchecked
+            {
+                return (Code.GetHashCode() * 397) ^ Description.GetHashCode();
+            }
+        }
+
+        protected override bool EqualsCore(Error other)
+        {
+            return Code == other.Code && Description == other.Description;
+        }
+
+    }
+}
