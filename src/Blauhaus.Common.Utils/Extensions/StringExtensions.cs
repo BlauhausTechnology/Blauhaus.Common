577d76da2458c53024590ff9287e4b5c316ebc23
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-04-10 11:16:53 +0200

    ExtractValueBetweenText

--- src/Blauhaus.Common.Utils/Extensions/StringExtensions.cs
+++ src/Blauhaus.Common.Utils/Extensions/StringExtensions.cs
@@ -4,11 +4,36 @@ namespace Blauhaus.Common.Utils.Extensions
 {
     public static class StringExtensions
     {
+        public static string ExtractValueBetweenText(this string stringContainingTags, string beginText, string endText)
+        {
+
+            var beginTextStartsAt = stringContainingTags.IndexOf(beginText, StringComparison.OrdinalIgnoreCase);
+            if (beginTextStartsAt == -1)
+            {
+                throw new ArgumentException($"Did not find expected beginning text of '{beginText}'");
+            }
+
+            var beginTextEndsAt = beginTextStartsAt + beginText.Length;
+
+            var endTextStartsAtIndex = stringContainingTags.IndexOf(endText, StringComparison.OrdinalIgnoreCase);
+            if (endTextStartsAtIndex == -1)
+            {
+                throw new ArgumentException($"Did not find expected ending text of '{beginText}'");
+            }
+
+
+            var payloadLength = endTextStartsAtIndex - beginTextEndsAt;
+
+            return stringContainingTags.Substring(beginTextEndsAt, payloadLength);
+
+        }   
+
         public static string ExtractValueBetweenTags(this string stringContainingTags, string tagName)
         {
             var openingTag = $"<{tagName}>";
             var closingTag = $"</{tagName}>";
 
+            return stringContainingTags.ExtractValueBetweenText(openingTag, closingTag);
             var openingTagStartsAtIndex= stringContainingTags.IndexOf(openingTag, StringComparison.Ordinal);
             if (openingTagStartsAtIndex == -1)
             {
