d4b27cedbcb08839f302bfc6106d90eb34b043e2
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-04-23 15:12:03 +0200

    no message

---
+++ src/Blauhaus.Common.Domain/Repositories/RepositoryErrors.cs
@@ -0,0 +1,10 @@
+﻿using Blauhaus.Common.Domain.Entities;
+using Blauhaus.Common.ValueObjects.Errors;
+
+namespace Blauhaus.Common.Domain.Repositories
+{
+    public static class RepositoryErrors
+    {
+        public static Error EntityNotFound<T>() => Error.Create($"{typeof(T).Name} entity was not found");
+    }
+}
