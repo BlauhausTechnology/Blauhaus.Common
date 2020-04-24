7bcaca7da1397b02ae9c64c2fb102f0dcedda5e2
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-04-24 12:23:55 +0200

    no message

--- src/Blauhaus.Common.Domain.TestHelpers/MockBuilders/CommandHandlers/BaseCommandHandlerMockBuilder.cs
+++ src/Blauhaus.Common.Domain.TestHelpers/MockBuilders/CommandHandlers/BaseCommandHandlerMockBuilder.cs
@@ -9,7 +9,7 @@ using Moq;
 
 namespace Blauhaus.Common.Domain.TestHelpers.MockBuilders.CommandHandlers
 {
-    public class CommandHandlerMockBuilder<TBuilder, TMock, TPayload, TCommand> : BaseMockBuilder<TBuilder, TMock>
+    public class BaseCommandHandlerMockBuilder<TBuilder, TMock, TPayload, TCommand> : BaseMockBuilder<TBuilder, TMock>
         where TMock : class, ICommandHandler<TPayload, TCommand> 
         where TBuilder : BaseMockBuilder<TBuilder, TMock>
     {
