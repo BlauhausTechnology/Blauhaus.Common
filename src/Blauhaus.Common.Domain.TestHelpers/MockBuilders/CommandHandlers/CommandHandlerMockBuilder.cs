d3cf15ea88735658c3854a2f7b73c8b84e698d87
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-04-24 12:20:42 +0200

    no message

---
+++ src/Blauhaus.Common.Domain.TestHelpers/MockBuilders/CommandHandlers/CommandHandlerMockBuilder.cs
@@ -0,0 +1,11 @@
+﻿using Blauhaus.Common.Domain.CommandHandlers;
+using Blauhaus.TestHelpers.MockBuilders;
+
+namespace Blauhaus.Common.Domain.TestHelpers.MockBuilders.CommandHandlers
+{
+    public class CommandHandlerMockBuilder<TPayload, TCommand> : BaseCommandHandlerMockBuilder<CommandHandlerMockBuilder<TPayload, TCommand>, 
+        ICommandHandler<TPayload, TCommand>, TPayload, TCommand>
+    {
+        
+    }
+}
