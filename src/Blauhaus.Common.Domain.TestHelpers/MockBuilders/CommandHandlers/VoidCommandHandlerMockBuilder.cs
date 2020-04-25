9eace2c5661f136617df25837af4112d641f32e3
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-04-25 13:36:31 +0200

    VoidCommandHandlerMockBuilder

---
+++ src/Blauhaus.Common.Domain.TestHelpers/MockBuilders/CommandHandlers/VoidCommandHandlerMockBuilder.cs
@@ -0,0 +1,12 @@
+﻿using Blauhaus.Common.Domain.CommandHandlers;
+using Blauhaus.Common.Domain.TestHelpers.MockBuilders.CommandHandlers._Base;
+using Blauhaus.TestHelpers.MockBuilders;
+
+namespace Blauhaus.Common.Domain.TestHelpers.MockBuilders.CommandHandlers
+{
+    public class VoidCommandHandlerMockBuilder<TCommand> : BaseVoidCommandHandlerMockBuilder<VoidCommandHandlerMockBuilder<TCommand>, 
+        IVoidCommandHandler<TCommand>, TCommand>
+    {
+        
+    }
+}
