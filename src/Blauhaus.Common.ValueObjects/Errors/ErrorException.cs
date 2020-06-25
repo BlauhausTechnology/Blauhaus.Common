5abbd4a34b63158877a15d840d7bbb9a3f669ba1
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-06-25 14:32:09 +0200

    ErrorException

---
+++ src/Blauhaus.Common.ValueObjects/Errors/ErrorException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Blauhaus.Common.ValueObjects.Errors
+{
+
+
+    /// <summary>
+    /// For all those times (cough RX cough) when you have to return an exception as the error condition
+    /// </summary>
+    public class ErrorException : Exception
+    {
+        public ErrorException(Error error)
+        {
+            Error = error;
+        }
+
+        public Error Error { get; }
+    }
+}
