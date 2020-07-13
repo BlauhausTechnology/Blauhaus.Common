10f40995c570da7e662f75c02d9dd12658f5cfa6
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-07-13 16:31:16 +0200

    ITaskExecutionDelayer

---
+++ src/Blauhaus.Common.Utils/ExecutionDelayer/ImmediateExecutor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Blauhaus.Common.Utils.ExecutionDelayer
+{
+    public class ImmediateTaskExecutor : ITaskExecutionDelayer
+    {
+        public async Task ExecuteAfterDelay(Func<Task> taskToExecuteAfterDelay, int delayMs)
+        {
+            //dummy for test purposes
+            await new TaskExecutionDelayer().ExecuteAfterDelay(taskToExecuteAfterDelay, 0);
+        }
+    }
+}
