73076e8537101dcf7630d8a04803bf7ba255d8b5
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-08-14 14:01:52 +0200

    no message

---
+++ src/Blauhaus.Common.Utils/Attributes/PreserveAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Blauhaus.Common.Utils.Attributes;
+
+[assembly:Preserve]
+
+namespace Blauhaus.Common.Utils.Attributes
+{
+   
+    [AttributeUsage(AttributeTargets.All)]
+    public class PreserveAttribute : Attribute 
+    {
+        public PreserveAttribute () {}
+        public bool Conditional { get; set; }
+
+
+    }
+}
