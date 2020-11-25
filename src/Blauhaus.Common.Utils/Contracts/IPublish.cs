e70c5a39d0494373ae4257f075eb93902a6521c2
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-11-25 18:04:59 +0200

    no message

---
+++ src/Blauhaus.Common.Utils/Contracts/IPublish.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Blauhaus.Common.Utils.Contracts
+{
+    public interface IPublish<out T>
+    {
+        Task<IDisposable> SubscribeAsync(Func<T, Task> handler);
+    }
+}
