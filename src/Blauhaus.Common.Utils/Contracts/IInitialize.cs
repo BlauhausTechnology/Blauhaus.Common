b5e3d7e852931d7120078ffdce9590c6bf5055ae
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-11-25 17:33:38 +0200

    no message

---
+++ src/Blauhaus.Common.Utils/Contracts/IInitialize.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+namespace Blauhaus.Common.Utils.Contracts
+{
+    public interface IInitialize
+    {
+        Task InitializeAsync();
+    }
+
+    public interface IInitialize<in T>
+    {
+        Task InitializeAsync(T value);
+    }
+}
