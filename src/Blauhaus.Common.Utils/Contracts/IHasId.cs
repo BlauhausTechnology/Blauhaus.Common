a5223adeb907afc9f9c656174073b339108ea7d4
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-02-16 12:59:00 +0200

    no message

--- src/Blauhaus.Common.Utils/Contracts/IHasId.cs
+++ src/Blauhaus.Common.Utils/Contracts/IHasId.cs
@@ -1,5 +1,10 @@
-﻿namespace Blauhaus.Common.Utils.Contracts
+﻿using System;
+
+namespace Blauhaus.Common.Utils.Contracts
 {
+
+    public interface IHasId : IHasId<Guid>{}
+
     public interface IHasId<out TId>
     {
         TId Id { get; }
---
+++ src/Blauhaus.Common.Utils/Contracts/IHasUserId.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Blauhaus.Common.Utils.Contracts
+{ 
+
+    public interface IHasUserId : IHasId<Guid>{}
+
+    public interface IHasUserId<out TId>
+    {
+        TId UserId { get; }
+    }
+}
