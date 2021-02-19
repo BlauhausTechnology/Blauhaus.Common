e98420bb7fe5092955324d526ee12b1630dfd297
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-02-19 14:18:01 +0200

    no message

---
+++ src/Blauhaus.Common.Utils/Contracts/IHasName.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blauhaus.Common.Utils.Contracts
+{
+    public interface IHasName
+    {
+        public string Name { get; }
+    }
+}
