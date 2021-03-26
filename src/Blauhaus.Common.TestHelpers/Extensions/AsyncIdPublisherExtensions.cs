c7ec81075bed4ef537c611465000971351d2bdb3
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-03-26 09:17:28 +0200

    no message

---
+++ src/Blauhaus.Common.TestHelpers/Extensions/AsyncIdPublisherExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Blauhaus.Common.Abstractions;
+
+namespace Blauhaus.Common.TestHelpers.Extensions
+{
+    public class PublishedIdModels<TUpdate, TId> : List<TUpdate>, IDisposable 
+    {
+        private readonly IDisposable? _token;
+
+        public PublishedIdModels(IAsyncIdPublisher<TUpdate, TId> bindableObject, TId id)
+        {
+            _token = Task.Run(async () => await bindableObject.SubscribeAsync(update =>
+            {
+                Add(update);
+                return Task.CompletedTask;
+            }, id));
+        }
+         
+        public void Dispose()
+        {
+            _token?.Dispose();
+        }
+    }
+    
+    public static class AsyncIdPublisherExtensions
+    {
+        public static PublishedIdModels<T, TId> SubscribeToUpdates<T, TId>(this IAsyncIdPublisher<T, TId> publisher, TId id)
+        {
+            return new PublishedIdModels<T, TId>(publisher, id);
+        } 
+    }
+}
---
+++ src/Blauhaus.Common.TestHelpers/Extensions/AsyncPublisherExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Blauhaus.Common.Abstractions;
+
+namespace Blauhaus.Common.TestHelpers.Extensions
+{
+    public class PublishedModels<TUpdate> : List<TUpdate>, IDisposable 
+    {
+        private readonly IDisposable? _token;
+
+        public PublishedModels(IAsyncPublisher<TUpdate> bindableObject)
+        {
+            _token = Task.Run(async () => await bindableObject.SubscribeAsync(update =>
+            {
+                Add(update);
+                return Task.CompletedTask;
+            }));
+        }
+         
+        public void Dispose()
+        {
+            _token?.Dispose();
+        }
+    }
+    
+    public static class AsyncPublisherExtensions
+    {
+        public static PublishedModels<T> SubscribeToUpdates<T>(this IAsyncPublisher<T> publisher)
+        {
+            return new PublishedModels<T>(publisher);
+        } 
+    }
+}
