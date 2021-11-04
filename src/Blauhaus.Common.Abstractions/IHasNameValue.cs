ad0edac96aff1b6f85733d618827beb05d18df57
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-11-04 12:05:05 +0200

    no message

--- src/Blauhaus.Common.Abstractions/IHasNameValue.cs
+++ src/Blauhaus.Common.Abstractions/IHasNameValue.cs
@@ -1,5 +1,9 @@
 ﻿namespace Blauhaus.Common.Abstractions
 {
+        
+    public interface IHasNameValue : IHasNameValue<string>
+    {
+    }
     public interface IHasNameValue<out TValue> : IHasName 
     {
         TValue Value { get; }
