4c47696a10581eb086845fcfa413db6852658e5b
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-11-16 07:42:26 +0200

    no message

--- src/Blauhaus.Common.ValueObjects/.Base/BaseNamedValueObject.cs
+++ src/Blauhaus.Common.ValueObjects/.Base/BaseNamedValueObject.cs
@@ -1,15 +1,28 @@
 ﻿using Blauhaus.Common.Abstractions;
+using System.Collections.Generic;
+using System;
 
 namespace Blauhaus.Common.ValueObjects.Base
 {
     public abstract class BaseNamedValueObject<T> : BaseValueObject<T>, IHasName where T : BaseNamedValueObject<T>
     {
+        
+        private static readonly Dictionary<string, Func<T>> FactoryFuncs = new();
+
         protected BaseNamedValueObject(string name)
         {
             Name = name;
+            FactoryFuncs[name] = () => (T)this;
         }
 
         public string Name { get; }
+
+        public static T FromName(string name)
+        {
+            return FactoryFuncs.TryGetValue(name, out var factory) 
+                ? factory.Invoke() 
+                : throw new InvalidOperationException($"This is no {typeof(T).Name} with a name of {name}");
+        }
 
         #region Equality
 
