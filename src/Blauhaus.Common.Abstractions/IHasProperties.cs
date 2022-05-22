c6122c78ceb7b34cca3384de2836e65c62fad059
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2022-05-22 14:07:15 +0200

    no message

---
+++ src/Blauhaus.Common.Abstractions/IHasProperties.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Blauhaus.Common.Abstractions
+{
+    public interface IHasProperties
+    {
+        public Dictionary<string, string> Properties { get; }
+    }
+}
