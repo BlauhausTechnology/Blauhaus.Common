cf5b1127cee82a9d4d05f821ccd3115dc410cfc9
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2022-08-01 07:51:40 +0200

    no message

---
+++ src/Blauhaus.Common.Abstractions/Extensions/HasPropertiesExtensions.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Blauhaus.Common.Abstractions.Extensions
+{
+    public static class HasPropertiesExtensions
+    {
+        
+        public static T AddProperties<T>(this T hasProperties, Dictionary<string, string> properties) where T : IHasProperties
+        {
+            foreach (var property in properties)
+            {
+                hasProperties.Properties[property.Key] = property.Value;
+            }
+
+            return hasProperties;
+        }
+
+        public static bool TryGetValue(this IHasProperties hasProperties, string key, out string value, bool allowEmpty = false)
+        {
+            foreach (var property in hasProperties.Properties)
+            {
+                if (string.Equals(property.Key, key, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    value = property.Value;
+                    if (allowEmpty)
+                    {
+                        return true;
+                    }
+
+                    return !string.IsNullOrEmpty(property.Value);
+                }
+            }
+
+            value = string.Empty;
+            return false;
+        }
+
+        public static bool TryGetId(this IHasProperties hasProperties, string key, out Guid value, bool allowEmpty = false) 
+        {
+            foreach (var property in hasProperties.Properties)
+            {
+                if (string.Equals(property.Key, key, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    if (Guid.TryParse(property.Value, out value))
+                    {
+                        if (allowEmpty)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            if (value != Guid.Empty)
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+
+            value = Guid.Empty;
+            return false;
+        }
+        
+        
+        public static T WithValue<T>(this T hasProperties, string key, string value) where T : IHasProperties
+        {
+            hasProperties.Properties[key] = value;
+            return hasProperties;
+        }
+    }
+}
---
+++ src/Blauhaus.Common.TestHelpers/Extensions/HasProptertiesExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Blauhaus.Common.Abstractions;
+using Blauhaus.Common.Abstractions.Extensions;
+using NUnit.Framework;
+
+namespace Blauhaus.Common.TestHelpers.Extensions
+{
+    public static class HasProptertiesExtensions
+    {
+        
+        public static bool VerifyValue(this IHasProperties hasProperties, string key, string expectedValue)
+        {
+            if (!hasProperties.Properties.TryGetValue(key, out var actualValue))
+            {
+                Assert.Fail($"No value found with key {key}");
+                return false;
+            } 
+
+            Assert.That(actualValue, Is.EqualTo(expectedValue));
+            return true;
+        }
+
+    
+        public static T RemoveProperty<T>(this T hasProperties, string key) where T : IHasProperties
+        {
+            if (hasProperties.Properties.TryGetValue(key, out var _))
+            {
+                hasProperties.Properties.Remove(key);
+            } 
+
+            return hasProperties;
+        }
+
+        public static T RemoveProperty<T>(this T hasProperties, Guid key) where T : IHasProperties
+        {
+            if (hasProperties.TryGetValue(key.ToString(), out var _))
+            {
+                hasProperties.Properties.Remove(key.ToString());
+            } 
+
+            return hasProperties;
+        }
+    }
+}
