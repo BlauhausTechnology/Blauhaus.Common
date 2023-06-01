25433b3e89e48e59f28339de35bca15b6b3f59a2
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2023-06-01 14:26:37 +0200

    no message

---
+++ src/Blauhaus.Common.Tests/Tests/ValueObjectTests/MeasuresTests/RatioTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Data;
+using Blauhaus.Common.ValueObjects.Measures;
+using NUnit.Framework;
+
+namespace Blauhaus.Common.Tests.Tests.ValueObjectTests.MeasuresTests;
+
+public class RatioTests
+{
+    [TestCase(-20, 0.01d)]
+    [TestCase(-10, 0.1d)]
+    [TestCase(10, 10d)]
+    [TestCase(20, 100d)]
+    [TestCase(30, 1000d)]
+    public void SHOULD_create_from_db(double db, double ratio)
+    {
+        //Act
+        var sut = Ratio.FromDecibels(db);
+
+        //Assert
+        Assert.That(sut.Value, Is.EqualTo(ratio));
+    }
+
+    [TestCase(-20, 0.01d)]
+    [TestCase(-10, 0.1d)]
+    [TestCase(10, 10d)]
+    [TestCase(20, 100d)]
+    [TestCase(30, 1000d)]
+    public void SHOULD_convert_to_db(double db, double ratio)
+    {
+        //Act
+        var sut = Ratio.Create(ratio);
+
+        //Assert
+        Assert.That(sut.Decibels, Is.EqualTo(db));
+    }
+
+}
---
+++ src/Blauhaus.Common.ValueObjects/Measures/Ratio.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.Json.Serialization;
+using Blauhaus.Common.ValueObjects.Base;
+
+namespace Blauhaus.Common.ValueObjects.Measures;
+
+public class Ratio : BaseDoubleValueObject<Ratio>
+{
+    [JsonConstructor]
+    public Ratio(double value) : base(value) { }
+
+
+    public double Decibels => 10 * Math.Log10(Value);
+
+
+    public static Ratio FromValues(double numerator, double denominator)
+    {
+        return new Ratio(numerator / denominator);
+    }
+
+    public static Ratio FromDecibels(double decibels)
+    {
+        return new Ratio(Math.Pow(10, decibels / 10d));
+    }
+     
+}
