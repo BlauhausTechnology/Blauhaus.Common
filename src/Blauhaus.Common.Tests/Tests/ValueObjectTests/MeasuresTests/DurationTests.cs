ece065a22380ce4e8c0afc4946b17a47f7f66958
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2023-06-02 08:55:41 +0200

    no message

---
+++ src/Blauhaus.Common.Tests/Tests/ValueObjectTests/MeasuresTests/DurationTests.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Blauhaus.Common.ValueObjects.Measures;
+using NUnit.Framework;
+
+namespace Blauhaus.Common.Tests.Tests.ValueObjectTests.MeasuresTests;
+
+public class DurationTests
+{
+    [Test]
+    public void SHOULD_convert_a_minute_from_ticks_correctly()
+    {
+        //Act
+        var timeSpan = TimeSpan.FromMinutes(1);
+        var sut = new Duration(timeSpan.Ticks);
+
+        //Assert
+        Assert.That(sut.Nanoseconds, Is.EqualTo(timeSpan.TotalNanoseconds));
+        Assert.That(sut.Ticks, Is.EqualTo(timeSpan.Ticks));
+        Assert.That(sut.Microseconds, Is.EqualTo(timeSpan.TotalMicroseconds));
+        Assert.That(sut.Milliseconds, Is.EqualTo(timeSpan.TotalMilliseconds));
+        Assert.That(sut.Seconds, Is.EqualTo(timeSpan.TotalSeconds));
+        Assert.That(sut.Minutes, Is.EqualTo(timeSpan.TotalMinutes));
+        Assert.That(sut.Hours, Is.EqualTo(timeSpan.TotalHours));
+        Assert.That(sut.Days, Is.EqualTo(timeSpan.TotalDays));
+    }
+
+    [Test]
+    public void SHOULD_convert_weird_time_from_ticks_correctly()
+    {
+        //Act
+        var timeSpan = new TimeSpan(1, 2, 3, 4, 5, 6);
+        var sut = new Duration(timeSpan.Ticks);
+
+        //Assert
+        Assert.That(sut.Nanoseconds, Is.EqualTo(timeSpan.TotalNanoseconds));
+        Assert.That(sut.Ticks, Is.EqualTo(timeSpan.Ticks));
+        Assert.That(sut.Microseconds, Is.EqualTo(timeSpan.TotalMicroseconds));
+        Assert.That(sut.Milliseconds, Is.EqualTo(timeSpan.TotalMilliseconds));
+        Assert.That(sut.Seconds, Is.EqualTo(timeSpan.TotalSeconds));
+        Assert.That(sut.Minutes, Is.EqualTo(timeSpan.TotalMinutes));
+        Assert.That(sut.Hours, Is.EqualTo(timeSpan.TotalHours));
+        Assert.That(sut.Days, Is.EqualTo(timeSpan.TotalDays));
+    }
+
+    [Test]
+    public void SHOULD_convert_timespans()
+    {
+        //Arrange
+        var timeSpan = new TimeSpan(1, 2, 3, 4, 5, 6);
+
+        //Act
+        var duration = Duration.FromTimespan(timeSpan);
+        var result = duration.ToTimeSpan;
+
+        //Assert
+        Assert.That(timeSpan, Is.EqualTo(result));
+    }
+
+    [Test]
+    public void SHOULD_convert_to_string()
+    {
+        Assert.That(Duration.FromTimespan(TimeSpan.FromDays(2.1)).ToString(), Is.EqualTo("2.1 days"));
+        Assert.That(Duration.FromTimespan(TimeSpan.FromHours(2.1)).ToString(), Is.EqualTo("2.1 hours"));
+        Assert.That(Duration.FromTimespan(TimeSpan.FromMinutes(2.1)).ToString(), Is.EqualTo("2.1 minutes"));
+        Assert.That(Duration.FromTimespan(TimeSpan.FromSeconds(2.1)).ToString(), Is.EqualTo("2.1 seconds"));
+        Assert.That(Duration.FromTimespan(TimeSpan.FromMilliseconds(2.1)).ToString(), Is.EqualTo("2.1 milliseconds"));
+        Assert.That(Duration.FromTimespan(TimeSpan.FromMicroseconds(2.1)).ToString(), Is.EqualTo("2.1 microseconds"));
+        Assert.That(Duration.FromTimespan(TimeSpan.FromTicks(2)).ToString(), Is.EqualTo("200 nanoseconds"));
+
+    }
+
+    [Test]
+    public void SHOULD_create()
+    {
+        Assert.That(Duration.FromDays(2.1).Ticks, Is.EqualTo(TimeSpan.FromDays(2.1).Ticks));
+        Assert.That(Duration.FromHours(2.1).Ticks, Is.EqualTo(TimeSpan.FromHours(2.1).Ticks));
+        Assert.That(Duration.FromMinutes(2.1).Ticks, Is.EqualTo(TimeSpan.FromMinutes(2.1).Ticks));
+        Assert.That(Duration.FromSeconds(2.1).Ticks, Is.EqualTo(TimeSpan.FromSeconds(2.1).Ticks));
+        Assert.That(Duration.FromMilliseconds(2.1).Ticks, Is.EqualTo(TimeSpan.FromMilliseconds(2.1).Ticks));
+        Assert.That(Duration.FromMicroseconds(2.1).Ticks, Is.EqualTo(TimeSpan.FromMicroseconds(2.1).Ticks));
+        Assert.That(Duration.FromNanoseconds(200).Ticks, Is.EqualTo(TimeSpan.FromTicks(2).Ticks));
+        Assert.That(Duration.FromTicks(200).Ticks, Is.EqualTo(TimeSpan.FromTicks(200).Ticks));
+
+    }
+
+
+
+
+}
---
+++ src/Blauhaus.Common.ValueObjects/Measures/Duration.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Globalization;
+using System.Text.Json.Serialization;
+using Blauhaus.Common.ValueObjects.Base;
+
+namespace Blauhaus.Common.ValueObjects.Measures;
+
+public class Duration : BaseDoubleValueObject<Duration>
+{
+    public Duration(double value) : base(value) { }
+
+
+    [JsonIgnore] public long Ticks => (long)Value;
+    [JsonIgnore] public double Nanoseconds => Value * 100;
+    [JsonIgnore] public double Microseconds => Value / 10;
+    [JsonIgnore] public double Milliseconds => Value / 10_000;
+    [JsonIgnore] public double Seconds => Value / 10_000_000;
+    [JsonIgnore] public double Minutes => Value / 600_000_000;
+    [JsonIgnore] public double Hours => Value / 36_000_000_000;
+    [JsonIgnore] public double Days => Value / 864_000_000_000;
+    
+
+    public static Duration FromTimespan(TimeSpan t) => new(t.Ticks);
+    public static Duration FromTicks(long ticks) => new(ticks);
+    public static Duration FromNanoseconds(double nanseconds) => new(nanseconds / 100d);
+    public static Duration FromMicroseconds(double microseconds) => new(microseconds * 10d);
+    public static Duration FromMilliseconds(double milliseconds) => new(milliseconds * 10_000d);
+    public static Duration FromSeconds(double seconds) => new(seconds * 10_000_000);
+    public static Duration FromMinutes(double minutes) => new(minutes * 600_000_000);
+    public static Duration FromHours(double hours) => new(hours * 36_000_000_000);
+    public static Duration FromDays(double days) => new(days * 864_000_000_000);
+
+
+
+    public TimeSpan ToTimeSpan => TimeSpan.FromTicks(Ticks);
+
+    public override string ToString()
+    {
+        if (Days > 1)
+            return $"{Math.Round(Days, 2).ToString(CultureInfo.InvariantCulture)} days";
+        if (Hours > 1)
+            return $"{Math.Round(Hours, 2).ToString(CultureInfo.InvariantCulture)} hours";
+        if (Minutes > 1)
+            return $"{Math.Round(Minutes, 2).ToString(CultureInfo.InvariantCulture)} minutes";
+        if (Seconds > 1)
+            return $"{Math.Round(Seconds, 2).ToString(CultureInfo.InvariantCulture)} seconds";
+        if (Milliseconds > 1)
+            return $"{Math.Round(Milliseconds, 2).ToString(CultureInfo.InvariantCulture)} milliseconds";
+        if (Microseconds > 1)
+            return $"{Math.Round(Microseconds, 2).ToString(CultureInfo.InvariantCulture)} microseconds";
+
+        return $"{Nanoseconds.ToString(CultureInfo.InvariantCulture)} nanoseconds";
+         
+    }
+}
