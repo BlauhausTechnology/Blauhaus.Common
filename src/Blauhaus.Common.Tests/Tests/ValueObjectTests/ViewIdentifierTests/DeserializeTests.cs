9f3f147f64f7d9f320662d678ebe036664481dc1
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2023-07-20 08:21:00 +0200

    no message

---
+++ src/Blauhaus.Common.Tests/Tests/ValueObjectTests/ViewIdentifierTests/DeserializeTests.cs
@@ -0,0 +1,63 @@
+﻿using Blauhaus.Common.ValueObjects.Navigation;
+using NUnit.Framework;
+
+// ReSharper disable ExplicitCallerInfoArgument
+
+namespace Blauhaus.Common.Tests.Tests.ValueObjectTests.ViewIdentifierTests;
+
+public class DeserializeTests
+{
+     
+    [Test]
+    public void IF_no_properties_SHOULD_return_only_name()
+    {
+        //Arrange
+        var sut = new ViewIdentifier("MyView");
+
+        //Act
+        var result = ViewIdentifier.Deserialize(sut.Serialize());
+
+        //Assert
+        Assert.That(result, Is.EqualTo(sut));
+    }
+
+    [Test]
+    public void IF_one_property_SHOULD_append()
+    {
+        //Arrange
+        var sut = new ViewIdentifier("MyView")
+        {
+            Properties =
+            {
+                ["one"] = "1"
+            }
+        };
+        
+        //Act
+        var result = ViewIdentifier.Deserialize(sut.Serialize());
+
+        //Assert
+        Assert.That(result, Is.EqualTo(sut));
+    }
+
+    [Test]
+    public void IF_multiple_properties_SHOULD_append()
+    {
+        //Arrange
+        var sut = new ViewIdentifier("MyView")
+        {
+            Properties =
+            {
+                ["one"] = "1",
+                ["two"] = "2"
+            }
+        };
+        
+        //Act
+        var result = ViewIdentifier.Deserialize(sut.Serialize());
+
+        //Assert
+        Assert.That(result, Is.EqualTo(sut));
+    }
+		
+}
---
+++ src/Blauhaus.Common.Tests/Tests/ValueObjectTests/ViewIdentifierTests/SerializeTests.cs
@@ -0,0 +1,54 @@
+﻿using Blauhaus.Common.ValueObjects.Navigation;
+using NUnit.Framework;
+
+// ReSharper disable ExplicitCallerInfoArgument
+
+namespace Blauhaus.Common.Tests.Tests.ValueObjectTests.ViewIdentifierTests;
+
+public class SerializeTests
+{
+     
+    [Test]
+    public void IF_no_properties_SHOULD_return_only_name()
+    {
+        //Arrange
+        var sut = new ViewIdentifier("MyView");
+
+        //Assert
+        Assert.That(sut.ToString(), Is.EqualTo("/MyView"));
+    }
+
+    [Test]
+    public void IF_one_property_SHOULD_append()
+    {
+        //Arrange
+        var sut = new ViewIdentifier("MyView")
+        {
+            Properties =
+            {
+                ["one"] = "1"
+            }
+        };
+
+        //Assert
+        Assert.That(sut.ToString(), Is.EqualTo("/MyView?one=1"));
+    }
+
+    [Test]
+    public void IF_multiple_properties_SHOULD_append()
+    {
+        //Arrange
+        var sut = new ViewIdentifier("MyView")
+        {
+            Properties =
+            {
+                ["one"] = "1",
+                ["two"] = "2"
+            }
+        };
+
+        //Assert
+        Assert.That(sut.ToString(), Is.EqualTo("/MyView?one=1&two=2"));
+    }
+		
+}
